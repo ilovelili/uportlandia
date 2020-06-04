8f8cae696f5e0e7a594e97b0f3ca3a41a921e3fb
ilovelili <route666@live.cn>
2020-06-04 14:30:18 +0900

    remove cloudfront

--- node_scripts/deploy_static.js
+++ node_scripts/deploy_static.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 const { argParse, run } = require("./helpers");
 const { config } = require("../setup_config");
 
-const didTemplate = url =>
-`{
+const didTemplate = (url) =>
+	`{
   "@context": "https://w3id.org/did/v1",
   "id": "did:https:${url}",
   "publicKey": [{
@@ -20,51 +20,52 @@ const didTemplate = url =>
 `;
 
 async function createDid(env) {
-  const url = config.domain[env];
-  return new Promise((resolve, reject) => {
-    if(!url) {
-      resolve();
-      return;
-    }
-    const PATH = "build/.well-known";
-    const doc = didTemplate(url);
-    if(!fs.existsSync(PATH)){
-      fs.mkdirSync(PATH);
-    }
-    fs.writeFile(`${PATH}/did.json`, doc, err => {
-      if(err) {
-        reject(err);
-      } else {
-        console.log("did.json created");
-        resolve();
-      }
-    });
-  });
+	const url = config.domain[env];
+	return new Promise((resolve, reject) => {
+		if (!url) {
+			resolve();
+			return;
+		}
+		const PATH = "build/.well-known";
+		const doc = didTemplate(url);
+		if (!fs.existsSync(PATH)) {
+			fs.mkdirSync(PATH);
+		}
+		fs.writeFile(`${PATH}/did.json`, doc, (err) => {
+			if (err) {
+				reject(err);
+			} else {
+				console.log("did.json created");
+				resolve();
+			}
+		});
+	});
 }
 
 async function deployStatic() {
-  const args = argParse();
-  // Build react app
-  process.env.REACT_APP_TARGET_ENV = args.env;
-  await run("yarn", [ "build" ]);
-  // Create did.json
-  await createDid(args.env);
-  // Upload to S3
-  await run("yarn", [
-    "s3-deploy",
-    "'./build/**'",
-    "'./build/.well-known/**'",
-    "--cwd",
-    "'./build/'",
-    "--region",
-    config.region,
-    "--bucket",
-    config.s3Bucket[args.env]
-  ]);
-  // Invalidate Cloudfront cache
-  if(config.domain[args.env]) {
-    await run("yarn", [ "cloudfront-invalidate-cache", "--cname", config.domain[args.env] ])
-  }
+	const args = argParse();
+	// Build react app
+	process.env.REACT_APP_TARGET_ENV = args.env;
+	await run("yarn", ["build"]);
+	// Create did.json
+	await createDid(args.env);
+	// Upload to S3
+	await run("yarn", [
+		"s3-deploy",
+		"'./build/**'",
+		"'./build/.well-known/**'",
+		"--cwd",
+		"'./build/'",
+		"--region",
+		config.region,
+		"--bucket",
+		config.s3Bucket[args.env],
+	]);
+
+	// // Invalidate Cloudfront cache
+	// if(config.domain[args.env]) {
+	//   await run("yarn", [ "cloudfront-invalidate-cache", "--cname", config.domain[args.env] ])
+	// }
 }
 
 deployStatic();
